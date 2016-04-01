d9442b331b24e856375536acb0fcda81946a935f
Bjorn S <bjorn@somnu.com>
2016-04-01 12:58:39 +0200

    Extended base Model with more features, fixed issues (get_class()) and cleaned up example.

--- includes/base/model.php
+++ includes/base/model.php
@@ -1,12 +1,12 @@
 <?php
-class Model {
+abstract class Model {
 
     public function toJson() {
 
     }
 
-    public function FillObject($row) {
-        $prefix = get_class($this);
+    public function fill($row) {
+        $prefix = get_called_class();
         $vars = get_class_vars($prefix);
         $check = array();
         foreach($vars as $k=>$v) {
@@ -28,6 +28,52 @@ class Model {
             error_log("Exception occured during FillObject()\n\r");
             error_log($e);
         }
+    }
+
+    public static function findById($id) {
+        $prefix = get_called_class();
+        $table = ucfirst($prefix."s");
+        try {
+            $db = DatabasePDO::start();
+            $result = $db->prepare("SELECT * FROM ".$table." WHERE ". ucfirst($prefix)."ID = :id");
+            $result->bindParam(":id", $id);
+            if(!@$result->execute()) {
+                throw new Exception("There's something wrong with the SQL query in 'Model->FindByID(".$id."). Model was used by '". $prefix."'");
+            }
+        } catch(Exception $e) {
+            error_log($e->getMessage());
+        }
+        if($result->rowCount()==1) {
+            $row = $result->fetch();
+            $obj = new static();
+            $obj->fill($row);
+            return $obj;
+        }
+        return false;
+    }
+
+    public static function findAll() {
+        $prefix = get_called_class();
+        $table = ucfirst($prefix."s");
+        try {
+            $db = DatabasePDO::start();
+            $result = $db->prepare("SELECT * FROM ".$table."");
+            $result->bindParam(":id", $id);
+            if(!@$result->execute()) {
+                throw new Exception("There's something wrong with the SQL query in 'Model->FindAll()'. Model was used by '". $prefix."'");
+            }
+            $array = array();
+            if($result->rowCount()>0) {
+                while($row = $result->fetch()) {
+                    $obj = new static();
+                    $obj->fill($row);
+                    array_push($array, $obj);
+                }
+            }
+        } catch(Exception $e) {
+            error_log($e->getMessage());
+        }
+        return $array;
     }
 
     public function getID() {
--- includes/models/example.php
+++ includes/models/example.php
@@ -9,5 +9,7 @@ class Example extends Model {
     public function __toString() {
         return $this->text;
     }
+
+    // You have access to all "Base Model" functions, including fill($obj), findAll(), findById(id) and more.
 }
 ?>
